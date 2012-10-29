ea5ace96ee6dc99f0268aa4f0f3c042d3108e509
F. Kooman <fkooman@tuxed.net>
2012-10-29 12:56:32 +0100

    new dependency installer

--- docs/install_dependencies.sh
+++ docs/install_dependencies.sh
@@ -2,6 +2,7 @@
 
 rm -rf ext
 mkdir -p ext/js
+mkdir -p ext/bootstrap
 
 # jQuery
 curl -o ext/js/jquery.js http://code.jquery.com/jquery.min.js
@@ -13,17 +14,7 @@ curl -o ext/js/jsrender.js https://raw.github.com/BorisMoore/jsrender/master/jsr
 curl -o ext/js/jso.js https://raw.github.com/andreassolberg/jso/master/jso.js
 
 # Bootstrap
-cd ext/
-curl -o bootstrap.zip http://twitter.github.com/bootstrap/assets/bootstrap.zip
-unzip -o -q bootstrap.zip
-rm bootstrap.zip
-cd ../
+curl -o ext/bootstrap.zip http://twitter.github.com/bootstrap/assets/bootstrap.zip
+(cd ext/ && unzip bootstrap.zip && rm bootstrap.zip)
 
-# Bootstrap Modal
-curl -o ext/js/bootstrap-modal.js http://twitter.github.com/bootstrap/assets/js/bootstrap-modal.js
 
-# Bootstrap Tooltip
-curl -o ext/js/bootstrap-tooltip.js http://twitter.github.com/bootstrap/assets/js/bootstrap-tooltip.js
-
-# Bootstrap Popover
-curl -o ext/js/bootstrap-popover.js http://twitter.github.com/bootstrap/assets/js/bootstrap-popover.js
