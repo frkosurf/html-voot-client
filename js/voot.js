696cc5585fc513699f0620d90b6a5ced035e5785
F. Kooman <fkooman@tuxed.net>
2012-10-30 11:42:17 +0100

    support pagination

--- js/voot.js
+++ js/voot.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    var pageLimit = 10
+;
     var apiScope = ["read"];
 
     jso_configure({
@@ -11,15 +13,27 @@ $(document).ready(function () {
     jso_ensureTokens({
         "html-voot-client": apiScope
     });
-    function renderGroupList() {
+
+    function renderGroupList(pageNumber) {
         $.oajax({
-            url: apiEndpoint + "/groups/@me",
+            url: apiEndpoint + "/groups/@me?startIndex=" + pageNumber + "&count=" + pageLimit,
             jso_provider: "html-voot-client",
             jso_scopes: apiScope,
             jso_allowia: true,
             dataType: 'json',
             success: function (data) {
                 $("#groupList").html($("#groupListTemplate").render(data.entry));
+                if(data.totalResults > pageLimit) {
+                    // show pagination stuff
+                    var x = [];
+                    var extraPage = (data.totalResults % pageLimit === 0) ? 0 : 1;
+                    for(var i = 0; i < data.totalResults / pageLimit + extraPage; i++) {
+                        x.push({'pageNumber': i});
+                    }
+                    //alert(JSON.stringify(x));
+                    $("#paginationList").html($("#paginationEntry").render(x));
+                    addPaginationHandlers();
+                }
                 addGroupListHandlers();
             }
         });
@@ -44,9 +58,15 @@ $(document).ready(function () {
         });
     }
 
+    function addPaginationHandlers() {
+        $("a.pageEntry").click(function () {
+            renderGroupList($(this).data('pageNumber')*pageLimit);
+        });
+    }
+
     function initPage() {
         $("#memberListModal").hide();
-        renderGroupList();
+        renderGroupList(0);
     }
     initPage();
 });
