0c24ac6584a932cd0bb1150ecefc9cb9e599b8f4
F. Kooman <fkooman@tuxed.net>
2012-10-30 20:26:26 +0100

    also implement paging for viewing group members

--- js/voot.js
+++ js/voot.js
@@ -27,36 +27,52 @@ $(document).ready(function () {
                     for(var i = 0; i < Math.ceil(data.totalResults / maxPageLength); i++) {
                         pages.push({'pageNumber': i});
                     }
-                    $("#paginationList").html($("#paginationEntry").render(pages));
-                    addPaginationHandlers();
+                    $("#groupPaginationList").html($("#paginationEntry").render(pages));
+                    addGroupPaginationHandlers();
                 }
                 addGroupListHandlers();
             }
         });
     }
 
-    function getGroupMembers(groupId) {
+    function renderPeopleList(groupId, pageNumber) {
         $.oajax({
-            url: apiEndpoint + "/people/@me/" + groupId,
+            url: apiEndpoint + "/people/@me/" + groupId + "?startIndex=" + pageNumber + "&count=" + maxPageLength,
             jso_provider: "html-voot-client",
             jso_scopes: apiScope,
             jso_allowia: true,
             dataType: 'json',
             success: function (data) {
                 $("#memberList").html($("#memberListTemplate").render(data.entry));
+                if(data.totalResults > maxPageLength) {
+                    // show pagination stuff
+                    var pages = [];
+                    for(var i = 0; i < Math.ceil(data.totalResults / maxPageLength); i++) {
+                        pages.push({'pageNumber': i});
+                    }
+                    $("#peoplePaginationList").html($("#paginationEntry").render(pages));
+                    addPeoplePaginationHandlers(groupId);
+                }
+                addGroupListHandlers();
             }
         });
     }
 
     function addGroupListHandlers() {
         $("a.groupEntry").click(function () {
-            getGroupMembers($(this).data('groupId'));
+            renderPeopleList($(this).data('groupId'), 0);
         });
     }
 
-    function addPaginationHandlers() {
+    function addGroupPaginationHandlers() {
         $("a.pageEntry").click(function () {
             renderGroupList($(this).data('pageNumber')*maxPageLength);
+        });
+    }
+
+    function addPeoplePaginationHandlers(groupId) {
+        $("a.pageEntry").click(function () {
+            renderPeopleList(groupId, $(this).data('pageNumber')*maxPageLength);
         });
     }
 
