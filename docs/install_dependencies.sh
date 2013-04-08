d70ae06fc80c962bbd87575563ba50da7bb8b1e7
François Kooman <fkooman@tuxed.net>
2013-04-08 13:34:54 +0200

    update install_dependencies.sh script

--- docs/install_dependencies.sh
+++ docs/install_dependencies.sh
@@ -5,16 +5,14 @@ mkdir -p ext/js
 mkdir -p ext/bootstrap
 
 # jQuery
-curl -o ext/js/jquery.js http://code.jquery.com/jquery.min.js
+curl -L -o ext/js/jquery.js http://code.jquery.com/jquery.min.js
 
 # JSrender (JavaScript Template Rendering for jQuery)
-curl -o ext/js/jsrender.js https://raw.github.com/BorisMoore/jsrender/master/jsrender.js
+curl -L -o ext/js/jsrender.js https://raw.github.com/BorisMoore/jsrender/master/jsrender.js
 
 # JSO (JavaScript OAuth 2 client)
-curl -o ext/js/jso.js https://raw.github.com/andreassolberg/jso/master/jso.js
+curl -L -o ext/js/jso.js https://raw.github.com/andreassolberg/jso/master/jso.js
 
 # Bootstrap
-curl -o ext/bootstrap.zip http://twitter.github.com/bootstrap/assets/bootstrap.zip
+curl -L -o ext/bootstrap.zip http://twitter.github.io/bootstrap/assets/bootstrap.zip
 (cd ext/ && unzip bootstrap.zip && rm bootstrap.zip)
-
-
